1096b1c1cb9c53736a7cceae25ee36019df34f66
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 15:50:45 +0200

    `NotGate` and tests

---
+++ Model.UnitTests/Components/NotGateTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Components;
+using Model.UnitTests.util;
+
+namespace Model.UnitTests.Components
+{
+	[TestClass]
+	public class NotGateTests
+	{
+		[TestMethod]
+		public void ShouldStartAsTrue()
+		{
+			Component notGate = new NotGate("testId");
+
+			Assert.IsTrue(notGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void FalseIsTrue()
+		{
+			Signal signal = new Signal("testA", 0);
+
+			Component notGate = new NotGate("testId");
+			notGate.SetInput(0, signal);
+
+			Assert.IsTrue(notGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void TrueIsFalse()
+		{
+			Signal signal = new Signal("testA", 0);
+			signal.Update(true);
+
+			Component notGate = new NotGate("testId");
+			notGate.SetInput(0, signal);
+
+			Assert.IsFalse(notGate.OutputSignals[0].Value);
+		}
+	}
+}
---
+++ Model/Components/NotGate.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Model.Components
+{
+	public class NotGate : Component
+	{
+		public static readonly string NotTypeId = "NOT";
+
+		public NotGate(string id) : base(id, NotTypeId, 1, 1) { }
+
+		protected override IEnumerable<bool> Logic(IEnumerable<bool> inputValues)
+		{
+			return new bool[] { !inputValues.ElementAt(0) };
+		}
+	}
+}
