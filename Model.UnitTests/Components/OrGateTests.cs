edcb596402c1b8fffadb14800626bf0214fb2292
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 15:44:07 +0200

    `OrGate` and tests

---
+++ Model.UnitTests/Components/OrGateTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Components;
+using Model.UnitTests.util;
+
+namespace Model.UnitTests.Components
+{
+	[TestClass]
+	public class OrGateTests
+	{
+		[TestMethod]
+		public void ShouldStartAsFalse()
+		{
+			Component andGate = new OrGate("testId", 2);
+
+			Assert.IsFalse(andGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void FalseAndFalseIsFalse()
+		{
+			Signal a = new Signal("testA", 0);
+
+			Signal b = new Signal("testB", 0);
+
+			Component andGate = new OrGate("testId", 2);
+			andGate.SetInput(0, a);
+			andGate.SetInput(1, b);
+
+			Assert.IsFalse(andGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void TrueAndFalseIsTrue()
+		{
+			Signal a = new Signal("testA", 0);
+			a.Update(true);
+
+			Signal b = new Signal("testB", 0);
+
+			Component andGate = new OrGate("testId", 2);
+			andGate.SetInput(0, a);
+			andGate.SetInput(1, b);
+
+			Assert.IsTrue(andGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void TrueAndTrueIsTrue()
+		{
+			Signal a = new Signal("testA", 0);
+			a.Update(true);
+
+			Signal b = new Signal("testB", 0);
+			b.Update(true);
+
+			Component andGate = new OrGate("testId", 2);
+			andGate.SetInput(0, a);
+			andGate.SetInput(1, b);
+
+			Assert.IsTrue(andGate.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void FalseAndTrueIsTrue()
+		{
+			Signal a = new Signal("testA", 0);
+
+			Signal b = new Signal("testB", 0);
+			a.Update(true);
+
+			Component andGate = new OrGate("testId", 2);
+			andGate.SetInput(0, a);
+			andGate.SetInput(1, b);
+
+			Assert.IsTrue(andGate.OutputSignals[0].Value);
+		}
+	}
+}
---
+++ Model/Components/OrGate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Model.Components
+{
+	public class OrGate : Component
+	{
+		public static readonly string OrTypeId = "OR";
+
+		public OrGate(string id, int inputSize) : base(id, OrTypeId, inputSize, 1) { }
+
+		protected override IEnumerable<bool> Logic(IEnumerable<bool> inputValues)
+		{
+			foreach (bool value in inputValues)
+			{
+				if (value)
+				{
+					return new bool[] { true };
+				}
+			}
+
+			return new bool[] { false };
+		}
+	}
+}
