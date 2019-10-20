6491ed213a2498502b387d190576785d0958c850
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 16:02:06 +0200

    `InputComponent` and tests

---
+++ Model.UnitTests/Components/InputComponentTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Components;
+
+namespace Model.UnitTests.Components
+{
+	[TestClass]
+	public class InputComponentTests
+	{
+		[TestMethod]
+		public void ShouldStartAsFalse()
+		{
+			Component inComponent = new InputComponent("testId");
+
+			Assert.IsFalse(inComponent.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void TrueIsTrue()
+		{
+			InputComponent inComponent = new InputComponent("testId");
+			inComponent.State = true;
+
+			Assert.IsTrue(inComponent.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void FalseIsFalse()
+		{
+			InputComponent inComponent = new InputComponent("testId");
+			inComponent.State = false;
+
+			Assert.IsFalse(inComponent.OutputSignals[0].Value);
+		}
+
+		[TestMethod]
+		public void ShouldUpdateListenersOnStateChange()
+		{
+			InputComponent inComponent = new InputComponent("testId");
+			bool hasListenerBeenInvoked = false;
+
+			inComponent.OutputSignals[0].AddListener(
+				"testListenerId",
+				updateId =>
+				{
+					hasListenerBeenInvoked = true;
+				}
+			);
+			inComponent.State = true;
+
+			Assert.IsTrue(hasListenerBeenInvoked, "The listeners of output should be called imeadietly after a state change.");
+		}
+
+		[TestMethod]
+		public void ShouldNotUpdateListenersOnStateChangeToSame()
+		{
+			InputComponent inComponent = new InputComponent("testId");
+			bool hasListenerBeenInvoked = false;
+
+			inComponent.OutputSignals[0].AddListener(
+				"testListenerId",
+				updateId =>
+				{
+					hasListenerBeenInvoked = true;
+				}
+			);
+			inComponent.State = false;
+
+			Assert.IsFalse(hasListenerBeenInvoked, "The listeners of output should not be called if the state is \"changed\" to what it already is.");
+		}
+	}
+}
---
+++ Model/Components/InputComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Components
+{
+	public class InputComponent : Component
+	{
+		public static readonly string InputTypeId = "INPUT";
+
+		private bool state;
+		public bool State
+		{
+			get => state;
+			set
+			{
+				if (state != value)
+				{
+					state = value;
+
+					Update(Guid.NewGuid().ToString());
+				}
+			}
+		}
+
+		public InputComponent(string id) : base(id, InputTypeId, 0, 1) { }
+
+		protected override IEnumerable<bool> Logic(IEnumerable<bool> inputValues)
+		{
+			return new bool[] { State };
+		}
+	}
+}
