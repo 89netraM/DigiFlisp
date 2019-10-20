297af31383874cdfa1dcf5e2f775b39c4834abde
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 16:38:04 +0200

    `ComponentFactory` and tests

---
+++ Model.UnitTests/Components/ComponentFactoryTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.UnitTests.Components
+{
+	[TestClass]
+	public class ComponentFactoryTests
+	{
+		[TestMethod]
+		public void CanCreateAllComponents()
+		{
+			foreach (string typeId in ComponentFactory.ComponentTypeIds)
+			{
+				Component component = ComponentFactory.CreateComponent(typeId);
+
+				Assert.IsNotNull(component);
+			}
+		}
+
+		[TestMethod]
+		public void CanCreateCorrectType()
+		{
+			Component component = ComponentFactory.CreateComponent(AndGate.AndTypeId);
+
+			Assert.IsInstanceOfType(component, typeof(AndGate));
+		}
+
+		[TestMethod]
+		public void CanCreateWithCustomId()
+		{
+			string customId = Guid.NewGuid().ToString();
+
+			Component component = ComponentFactory.CreateComponent(AndGate.AndTypeId, customId);
+
+			Assert.AreEqual(customId, component.Id);
+		}
+
+		[TestMethod, ExpectedException(typeof(ArgumentException))]
+		public void ShouldThrowIfMissingTypeId()
+		{
+			_ = ComponentFactory.CreateComponent("MissingTypeId");
+		}
+	}
+}
---
+++ Model/Components/ComponentFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Components
+{
+	public delegate Component ComponentFactoryMethod(string id);
+
+	public class ComponentFactory
+	{
+		private readonly static IReadOnlyDictionary<string, ComponentFactoryMethod> componentCreatorMethods = new Dictionary<string, ComponentFactoryMethod>()
+		{
+			[AndGate.AndTypeId] = id => new AndGate(id, 2),
+			[OrGate.OrTypeId] = id => new OrGate(id, 2),
+			[NotGate.NotTypeId] = id => new NotGate(id),
+			[InputComponent.InputTypeId] = id => new InputComponent(id),
+			[OutputComponent.OutputTypeId] = id => new OutputComponent(id)
+		};
+
+		public static IEnumerable<string> ComponentTypeIds => componentCreatorMethods.Keys;
+
+		public static Component CreateComponent(string typeId)
+		{
+			return CreateComponent(typeId, Guid.NewGuid().ToString());
+		}
+		public static Component CreateComponent(string typeId, string id)
+		{
+			if (!componentCreatorMethods.ContainsKey(typeId))
+			{
+				throw new ArgumentException(nameof(typeId), "The given type id ´does not exist.");
+			}
+
+			return componentCreatorMethods[typeId].Invoke(id);
+		}
+	}
+}
