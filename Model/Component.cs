1fd85485a5b623c75dfe2f612b6873c5e546fc65
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 12:32:44 +0200

    Created `Component` with `Signal`

---
+++ Model/Component.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Model
+{
+	public abstract class Component
+	{
+		public string Id { get; }
+		public string TypeId { get; }
+
+		private readonly Signal[] inputSignals;
+		private readonly Signal[] outputSignals;
+
+		public Component(string id, string typeId, int inputSize, int outputSize)
+		{
+			Id = id;
+			TypeId = typeId;
+
+			inputSignals = new Signal[inputSize];
+			outputSignals = (new Signal[outputSize]).Select((ignored, index) => new Signal(Id, index)).ToArray();
+		}
+
+		public void SetInput(int index, Signal input)
+		{
+			Signal oldInput = inputSignals[index];
+
+			if (oldInput != input)
+			{
+				if (oldInput != null)
+				{
+					oldInput.RemoveListener(Id + index);
+				}
+
+				if (input != null)
+				{
+					inputSignals[index] = input;
+					input.AddListener(Id + index, Update);
+
+					Update(Guid.NewGuid().ToString());
+				}
+			}
+		}
+
+		public IReadOnlyCollection<Signal> GetInputSignals()
+		{
+			return inputSignals;
+		}
+
+		protected void Update(string updateId)
+		{
+			IEnumerable<bool> outputValues = Logic(inputSignals.Select(x => x.Value));
+
+			if (outputValues.Count() != outputSignals.Length)
+			{
+				throw new Exception($"The `{nameof(Logic)}` method must return the correct amount of outputs.");
+			}
+
+			for (int i = 0; i < outputSignals.Length; i++)
+			{
+				outputSignals[i].Update(updateId, outputValues.ElementAt(i));
+			}
+		}
+
+		protected abstract IEnumerable<bool> Logic(IEnumerable<bool> inputValues);
+	}
+}
---
+++ Model/Signal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+	public delegate void SignalListener(string updateId);
+
+	public sealed class Signal
+	{
+		public string OwnerId { get; }
+		public int Index { get; }
+
+		private readonly Dictionary<string, SignalListener> listeners;
+
+		private string lastUpdateId;
+		public bool Value { get; private set; }
+
+		public Signal(string ownerId, int index)
+		{
+			OwnerId = ownerId;
+			Index = index;
+		}
+
+		public void AddListener(string listenerId, SignalListener listener)
+		{
+			listeners.Add(listenerId, listener);
+		}
+
+		public void RemoveListener(string listenerId)
+		{
+			listeners.Remove(listenerId);
+		}
+
+		internal void Update(string updateId, bool newValue)
+		{
+			if (updateId == null)
+			{
+				throw new ArgumentNullException(nameof(updateId));
+			}
+			else if (updateId != lastUpdateId)
+			{
+				lastUpdateId = updateId;
+
+				Value = newValue;
+				InvokeListeners(updateId);
+			}
+		}
+
+		private void InvokeListeners(string updateId)
+		{
+			foreach (SignalListener listener in listeners.Values)
+			{
+				listener?.Invoke(updateId);
+			}
+		}
+	}
+}
