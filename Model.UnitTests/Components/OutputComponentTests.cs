15786289abbda4309ecaa5956a19906efae5e0df
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 16:18:42 +0200

    `OutputComponent` and tests

---
+++ Model.UnitTests/Components/OutputComponentTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Components;
+using Model.UnitTests.util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.UnitTests.Components
+{
+	[TestClass]
+	public class OutputComponentTests
+	{
+		[TestMethod]
+		public void ShoudlStartAsFalse()
+		{
+			OutputComponent outComponent = new OutputComponent("testId");
+
+			Assert.IsFalse(outComponent.State);
+		}
+
+		[TestMethod]
+		public void FalseIsFalse()
+		{
+			Signal signal = new Signal("testA", 0);
+
+			OutputComponent outComponent = new OutputComponent("testId");
+			outComponent.SetInput(0, signal);
+
+			Assert.IsFalse(outComponent.State);
+		}
+
+		[TestMethod]
+		public void TrueIsTrue()
+		{
+			Signal signal = new Signal("testA", 0);
+			signal.Update(true);
+
+			OutputComponent outComponent = new OutputComponent("testId");
+			outComponent.SetInput(0, signal);
+
+			Assert.IsTrue(outComponent.State);
+		}
+
+		[TestMethod]
+		public void ShouldUpdateListeners()
+		{
+			Signal signal = new Signal("testA", 0);
+			OutputComponent outComponent = new OutputComponent("testId");
+			outComponent.SetInput(0, signal);
+			bool hasListenerBeenInvoked = false;
+
+			outComponent.IndicatorSignal.AddListener(
+				"testListenerId",
+				updateId =>
+				{
+					hasListenerBeenInvoked = true;
+				}
+			);
+			signal.Update(true);
+
+			Assert.IsTrue(hasListenerBeenInvoked, "The listeners of output should be called imeadietly after a state change.");
+		}
+	}
+}
---
+++ Model/Components/OutputComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Model.Components
+{
+	public class OutputComponent : Component
+	{
+		public static readonly string OutputTypeId = "OUTPUT";
+
+		public readonly Signal IndicatorSignal;
+
+		private bool state;
+		public bool State
+		{
+			get => state;
+			private set
+			{
+				if (state != value)
+				{
+					state = value;
+
+					IndicatorSignal.Update(Guid.NewGuid().ToString(), value);
+				}
+			}
+		}
+
+		public OutputComponent(string id) : base(id, OutputTypeId, 1, 0)
+		{
+			IndicatorSignal = new Signal(Id, -1);
+		}
+
+		protected override IEnumerable<bool> Logic(IEnumerable<bool> inputValues)
+		{
+			State = inputValues.ElementAt(0);
+
+			return new bool[0];
+		}
+	}
+}
