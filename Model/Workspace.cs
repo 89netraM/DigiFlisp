6c11a3f1168872bf8f384fbee0624017607caf02
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-20 18:36:13 +0200

    Added `Workspace`

---
+++ Model/Workspace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+	public class Workspace
+	{
+		private readonly Dictionary<string, Blueprint> blueprintList = new Dictionary<string, Blueprint>();
+
+		public IReadOnlyCollection<string> BlueprintIds => blueprintList.Keys;
+
+		public Blueprint GetBlueprint(string id)
+		{
+			return blueprintList[id];
+		}
+
+		public void AddBlueprint(string id, Blueprint blueprint)
+		{
+			blueprintList.Add(id, blueprint);
+		}
+
+		public void RemoveBlueprint(string id)
+		{
+			blueprintList.Remove(id);
+		}
+	}
+}
