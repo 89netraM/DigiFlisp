9c429dc9021a2b88a9e7d633ebd4baca5d7ac829
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-28 10:03:16 +0100

    GUI: Infinite canvas with blocks

---
+++ GUI/Views/Infinite/InfiniteCanvas.cs
@@ -0,0 +1,126 @@
+﻿using Avalonia;
+using Avalonia.Input;
+using Avalonia.Controls;
+using System;
+
+namespace GUI.Views.Infinite
+{
+	public class InfiniteCanvas : Canvas
+	{
+		#region Child properties
+
+		public static readonly AttachedProperty<Point> PositionProperty = AvaloniaProperty.RegisterAttached<InfiniteCanvas, InfiniteCanvasBlock, Point>("Position");
+
+		public static readonly AttachedProperty<Size> SizeProperty = AvaloniaProperty.RegisterAttached<InfiniteCanvas, InfiniteCanvasBlock, Size>("Size");
+
+		static InfiniteCanvas()
+		{
+			AffectsParentArrange<InfiniteCanvas>(PositionProperty, SizeProperty);
+		}
+
+		public static Point GetPosition(InfiniteCanvasBlock block)
+		{
+			return block.GetValue(PositionProperty);
+		}
+
+		public static void SetPosition(InfiniteCanvasBlock block, Point value)
+		{
+			block.SetValue(PositionProperty, new Point(Math.Round(value.X), Math.Round(value.Y)));
+		}
+
+		public static Size GetSize(InfiniteCanvasBlock block)
+		{
+			return block.GetValue(SizeProperty);
+		}
+
+		public static void SetSize(InfiniteCanvasBlock block, Size value)
+		{
+			block.SetValue(SizeProperty, new Size(Math.Round(value.Width), Math.Round(value.Height)));
+		}
+
+		#endregion
+
+		private static readonly double[] zoomPoints = { 0.125d, 0.25d, 0.5d, 1.0d, 1.5d, 2.0d };
+		private static readonly int zoomPointBase = 3;
+
+		private static readonly double coordinateSize = 10.0d;
+
+		protected Point offset = new Point(0, 0);
+		private Point? lastPointerMove = null;
+
+		private int zoomPoint = zoomPointBase;
+		private double zoomFactor => zoomPoints[zoomPoint];
+
+		public InfiniteCanvas()
+		{
+			PointerPressed += InfiniteCanvas_PointerPressed;
+			PointerMoved += InfiniteCanvas_PointerMoved;
+			PointerReleased += InfiniteCanvas_PointerReleased;
+			PointerWheelChanged += InfiniteCanvas_PointerWheelChanged;
+
+			InvalidateArrange();
+		}
+
+		private void InfiniteCanvas_PointerPressed(object sender, PointerPressedEventArgs e)
+		{
+			if (e.MouseButton == MouseButton.Middle)
+			{
+				lastPointerMove = e.GetPosition(this);
+			}
+		}
+
+		private void InfiniteCanvas_PointerMoved(object sender, PointerEventArgs e)
+		{
+			if (e.InputModifiers == InputModifiers.MiddleMouseButton && lastPointerMove.HasValue)
+			{
+				Point nextPointerMove = e.GetPosition(this);
+				offset += nextPointerMove - lastPointerMove.Value;
+				lastPointerMove = nextPointerMove;
+
+				InvalidateArrange();
+			}
+		}
+
+		private void InfiniteCanvas_PointerReleased(object sender, PointerReleasedEventArgs e)
+		{
+			if (e.MouseButton == MouseButton.Middle)
+			{
+				lastPointerMove = null;
+			}
+		}
+
+		private void InfiniteCanvas_PointerWheelChanged(object sender, PointerWheelEventArgs e)
+		{
+			double zoomFactorBefore = zoomFactor;
+			Point offsetBefore = offset;
+			Point pointer = e.GetPosition(this);
+
+			zoomPoint = Math.Max(0, Math.Min(zoomPoint + (int)(e.Delta.Y / Math.Abs(e.Delta.Y)), zoomPoints.Length - 1));
+
+			offset = pointer - ((pointer - offsetBefore) * (zoomFactor / zoomFactorBefore));
+
+			InvalidateArrange();
+		}
+
+		protected override Size ArrangeOverride(Size arrangeSize)
+		{
+			foreach (IControl child in Children)
+			{
+				if (child is InfiniteCanvasBlock childBlock)
+				{
+					Size originalSize = childBlock.Bounds.Size;
+					Size zoomedSize = originalSize * zoomFactor;
+					Vector halfDimensions = new Vector(zoomedSize.Width - originalSize.Width, zoomedSize.Height - originalSize.Height) / 2.0d;
+					Point position = offset + GetPosition(childBlock) * coordinateSize;
+
+					Size size = GetSize(childBlock) * coordinateSize;
+
+					childBlock.Arrange(new Rect(position, size));
+					childBlock.Scale.ScaleX = childBlock.Scale.ScaleY = zoomFactor;
+				}
+			}
+
+			return arrangeSize;
+		}
+	}
+}
---
+++ GUI/Views/Infinite/InfiniteCanvasBlock.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+
+namespace GUI.Views.Infinite
+{
+	public class InfiniteCanvasBlock : UserControl
+	{
+		private Point pointerMoveStart;
+		private Point coordinateMoveStart;
+
+		public ScaleTransform Scale { get; } = new ScaleTransform(1.0d, 1.0d);
+
+		public InfiniteCanvasBlock()
+		{
+			RenderTransformOrigin = new RelativePoint(0.5d, 0.5d, RelativeUnit.Relative);
+			TransformGroup transforms = new TransformGroup();
+			if (RenderTransform != null)
+			{
+				transforms.Children.Add(RenderTransform);
+			}
+			transforms.Children.Add(Scale);
+			RenderTransform = transforms;
+
+			PointerPressed += InfiniteCanvasBlock_PointerPressed;
+			PointerMoved += InfiniteCanvasBlock_PointerMoved;
+		}
+
+		private void InfiniteCanvasBlock_PointerPressed(object sender, PointerPressedEventArgs e)
+		{
+			if (e.MouseButton == MouseButton.Left)
+			{
+				pointerMoveStart = Bounds.Center;
+				coordinateMoveStart = InfiniteCanvas.GetPosition(this);
+			}
+		}
+
+		private void InfiniteCanvasBlock_PointerMoved(object sender, PointerEventArgs e)
+		{
+			if (e.InputModifiers == InputModifiers.LeftMouseButton)
+			{
+				Point pointer = e.GetPosition(Parent);
+				Point delta = pointer - pointerMoveStart;
+
+				Size actualSize = Bounds.Size;
+				Size infiniteCanvasSize = InfiniteCanvas.GetSize(this);
+				Point deltaPosition = new Point(
+					delta.X / (actualSize.Width / infiniteCanvasSize.Width) / Scale.ScaleX,
+					delta.Y / (actualSize.Height / infiniteCanvasSize.Height) / Scale.ScaleY
+				);
+
+				InfiniteCanvas.SetPosition(this, coordinateMoveStart + deltaPosition);
+			}
+		}
+	}
+}
