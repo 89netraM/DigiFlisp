1aaef8b9a750d8c026f62b8f17a67a6b20018fbb
Mårten Åsberg <marten.asberg@outlook.com>
2020-03-19 14:26:21 +0100

    GUI: Component component
    With bindings for size and position

---
+++ GUI/ViewModels/ComponentViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Avalonia;
+using System;
+using ReactiveUI;
+using Model;
+
+namespace GUI.ViewModels
+{
+	public class ComponentViewModel : ViewModelBase
+	{
+		private readonly Component model;
+
+		public Point Position
+		{
+			get => new Point(model.Position.X, model.Position.Y);
+			set
+			{
+				if (model.Position.X != value.X && model.Position.Y != value.Y)
+				{
+					model.Position.X = Convert.ToInt32(value.X);
+					model.Position.Y = Convert.ToInt32(value.Y);
+					this.RaisePropertyChanged();
+				}
+				else if (model.Position.X != value.X)
+				{
+					model.Position.X = Convert.ToInt32(value.X);
+					this.RaisePropertyChanged();
+				}
+				else if (model.Position.Y != value.Y)
+				{
+					model.Position.Y = Convert.ToInt32(value.Y);
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		public Size Size { get; }
+
+		public ComponentViewModel(Component model)
+		{
+			this.model = model ?? throw new ArgumentNullException(nameof(model));
+			this.model.Position.ChangeEvent += Position_ChangeEvent;
+
+			int maxNrConnections = Math.Max(this.model.InputSignals.Count, this.model.OutputSignals.Count);
+			Size = new Size(5, 3 + 2 * (maxNrConnections - 1));
+		}
+
+		private void Position_ChangeEvent(object sender, PositionEvent e)
+		{
+			this.RaisePropertyChanged(nameof(Position));
+		}
+	}
+}
---
+++ GUI/Views/Components/Component.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Avalonia.Markup.Xaml;
+using GUI.ViewModels;
+using GUI.Views.Infinite;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GUI.Views.Components
+{
+	public class Component : InfiniteCanvasBlock
+	{
+		private readonly ComponentDataContext context = new ComponentDataContext();
+
+		public new object Content
+		{
+			get => context.Content;
+			set => context.Content = value;
+		}
+		public ComponentViewModel Model
+		{
+			get => context.Model;
+			set => context.Model = value;
+		}
+
+		public Component()
+		{
+			this.InitializeComponent();
+			DataContext = context;
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+
+	class ComponentDataContext : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private object content;
+		public object Content
+		{
+			get => content;
+			set => SetAndNotify(ref content, value);
+		}
+
+		private ComponentViewModel model;
+		public ComponentViewModel Model
+		{
+			get => model;
+			set => SetAndNotify(ref model, value);
+		}
+
+		private void SetAndNotify<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+		{
+			if (!Object.Equals(property, value))
+			{
+				property = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
